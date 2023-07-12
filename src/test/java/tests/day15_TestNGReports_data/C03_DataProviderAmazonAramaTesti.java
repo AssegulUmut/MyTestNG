295b3134a8fa54dd785deb9729120cf274f978af
aysegulUmut <assegul.kotay@gmail.com>
2023-07-12 13:30:16 +0300

    tem_12

--- src/test/java/tests/day15_TestNGReports_data/C03_DataProviderAmazonAramaTesti.java
+++ src/test/java/tests/day15_TestNGReports_data/C03_DataProviderAmazonAramaTesti.java
@@ -1,20 +1,30 @@
 package tests.day15_TestNGReports_data;
 
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.AmazonPage;
 import utilities.ConfigReader;
 import utilities.Driver;
-
 public class C03_DataProviderAmazonAramaTesti {
-    @Test
-    public void cokluAramaTesti(){
-
-        //amazon anasayfaya gidin
+    @DataProvider
+    public static Object[][] AranacakUrunListesi() {
+        //Dataprovider notasyonuna sahip method'lar
+        // ciftkatli array dondurmek zorundadir
+        String[][] urunList = {{"Nutella"}, {"Cokokrem"}, {"Java"}, {"Selenium"}, {"Apple"}, {"Samsung"}};
+        return urunList;
+    }
+    @Test(dataProvider = "AranacakUrunListesi")
+    public void cokluAramaTesti(String aranacakUrun){
+        // amazon anasayfaya gidin
         Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
-        //verilen listedeki her bir urun icin ayri ayri
-        //arama yapip, sonuclarin o urunu icerdigni test edin
-        //Nutella, Cokukrem, Java, Selenium, Apple, Samsunug
-        AmazonPage amazonPage=new AmazonPage();
-        amazonPage.aramaKutusu.sendKeys();
+        // verilen listedeki her bir urun icin ayri ayri
+        // arama yapip , sonuclarin o urunu icerdigini test edin
+        // Nutella, Cokokrem, Java, Selenium, Apple, Samsung
+        AmazonPage amazonPage = new AmazonPage();
+        amazonPage.aramaKutusu.sendKeys(aranacakUrun+ Keys.ENTER);
+        Assert.assertTrue(amazonPage.sonucYaziElementi.getText().contains(aranacakUrun));
+        Driver.closeDriver();
     }
 }
